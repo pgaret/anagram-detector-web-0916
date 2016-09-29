9b363442518e84762d65d0164d2139aaf06b7cc0
Peregrin <peregringaret@gmail.com>
2016-09-29 14:52:39 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        word = @word.chars
+        arr.each_with_object([]) do |words, returns|
+            if words.chars.sort == word.sort
+                returns.push(words)
+            end
+        end
+    end
+end
